cb6b3ec10d814f990f9bdff0e2b66692617d8d7b
Baptiste Hardelin <baptiste.hardelin.etu@univ-lille.fr>
2021-12-28 22:20:26 +0100

    Chnagement arborescence puis nesting component

---
+++ vue-js-tuto/src/Ninjas.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+        <li v-for="ninja in ninjas" v-bind:key="ninja">{{ninja}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ninjas: ['Yoshi', 'Mario', 'Ryu']
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
